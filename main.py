a40c95c22564e95eaee57908951006813762602b
nabil.lahssini <nabil.lahssini@student.ehb.be>
2021-08-19 00:54:46 +0200

    Error handling + clean code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import requests, sys, os, getopt
+"""Prayer assistant"""
+import sys
+import os
+import getopt
 from datetime import time, date, datetime
+from json import JSONDecodeError
+import requests
 from gtts import gTTS
 from playsound import playsound
 from googletrans import Translator
@@ -8,95 +13,96 @@ from googletrans import Translator
 translator = Translator()
 
 ##Declaring needed variables
-salat_list = ["Fajr", "Dhuhr", "Asr", "Maghreb", "Isha"] 
-school = 3
-current_time = datetime.now().timestamp()
-#current_time = 1629310000.29174 
+salat_list = ["Fajr","Dhuhr","Asr","Maghreb","Isha"]
+SCHOOL = 3
+CURRENT_TIME = datetime.now().timestamp()
+#CURRENT_TIME = 1629310000.29174
 URL = "https://api.pray.zone/v2/times/today.json"
-location = "Brussels"
-dest = "fr"
-help_string = '\nArguments:\n -c <city> (default -> Brussels) \n -l <language> (default -> fr)\n'
+LOCATION = "Brussels"
+DEST = "fr"
+HELP_STRING = '\nArguments:\n -c <city> (default -> Brussels) \n -l <language> (default -> fr)\n'
+ERROR_LINE = "An error occured, check for typos in the command line arguments or try again later"
 
 ##Getting all the command line arguments
 argv = sys.argv[1:]
 try:
     opts, args = getopt.getopt(argv,"hc:l:",["city=","lang="])
 except getopt.GetoptError:
-    print (help_string)
+    print (HELP_STRING)
     sys.exit(2)
 for opt, arg in opts:
     if opt in ("-h", "--help"):
-        print (help_string)
+        print (HELP_STRING)
         sys.exit()
     elif opt in ("-c", "--city"):
-        location = arg
+        LOCATION = arg
     elif opt in ("-l", "--lang"):
-        dest = arg
+        DEST = arg
 
-##Creates the request with correct url and parameters
-def requestBuilder():
-    PARAMS = {'city': location, 'school': school}
-    r = requests.get(url = URL, params= PARAMS)
-    return r
+def request_builder():
+    """Creates the request with correct url and parameters"""
+    params = {'city': LOCATION, 'school': SCHOOL}
+    request = requests.get(url = URL, params=params)
+    return request
 
-##makes the get request and returns raw json
-def getJson():
-    request = requestBuilder()
+def get_json():
+    """makes the get request and returns raw json"""
+    request = request_builder()
     data = request.json()
     return data
 
-##Format string hours to time object
-def formatTime(tempSalat):
-    (hour, second) = tempSalat.split(':')
+def format_time(temp_salat):
+    """Format string hours to time object"""
+    (hour, second) = temp_salat.split(':')
     return time(int(hour), int(second))
 
-##Returns timestamps from given time parameter
-def getTimeStamps(time):
+def get_timestamps(input_time):
+    """Returns timestamps from given time parameter"""
     tod = date.today()
-    today = datetime(tod.year, tod.month, tod.day, time.hour, time.minute, time.second)
+    today = datetime(tod.year, tod.month, tod.day,
+    input_time.hour, input_time.minute, input_time.second)
     return datetime.timestamp(today)
 
-##Uses the raw JSON to create an array of timestamps
-def parseJsonToTimestampArray():
-    times = getJson()["results"]["datetime"][0]["times"]
+def parse_json_to_timestamp_array():
+    """Uses the raw JSON to create an array of timestamps"""
+    times = get_json()["results"]["datetime"][0]["times"]
     salat_array = [times["Fajr"], times["Dhuhr"], times["Asr"], times["Maghrib"], times["Isha"]]
     time_array = []
     for sal in salat_array:
-        time_array.append(getTimeStamps(formatTime(sal)))
+        time_array.append(get_timestamps(format_time(sal)))
     return time_array
 
-##Will compare current time with the salat to show the next one
-def checkTime():
-    times = parseJsonToTimestampArray()
+def check_time():
+    """Will compare current time with the salat to show the next one"""
+    times = parse_json_to_timestamp_array()
     i = 0
     while i < 5:
-        if current_time < times[i]:
+        if CURRENT_TIME < times[i]:
             return datetime.fromtimestamp(times[i]), int(i)
-        else:
-            i += 1
+        i += 1
     return 0, 0
 
-##Translates the output
-def getTranslation(ins, source, dest):
-    translated = translator.translate(ins,src=source ,dest=dest)
+def get_translation(ins, source):
+    """Translates the output"""
+    translated = translator.translate(ins,src=source ,dest=DEST)
     return translated
 
-##Plays the sound
 def play(text):
+    """Plays the sound"""
     dire = "./out.mp3"
-    output = getTranslation(text, "fr", dest)
+    output = get_translation(text, "fr")
 	#Use the translated text to generate an mp3 file with it
-    gTTS(output.text, lang=dest).save(dire)
+    gTTS(output.text, lang=DEST).save(dire)
 	#Plays the mp3 file
     playsound(dire)
     return dire
 
-##Will generate a string and play it in the STT class
 def speak():
-    next_salat, number = checkTime()
+    """Will generate a string and play it in the STT class"""
+    next_salat, number = check_time()
     text = ""
     if next_salat != 0:
-        difference = next_salat - datetime.fromtimestamp(current_time)
+        difference = next_salat - datetime.fromtimestamp(CURRENT_TIME)
         sec = difference.total_seconds()
         result = ""
         minutes = int(sec/60)
@@ -107,19 +113,25 @@ def speak():
         else:
             result = str(minutes) + " minutes"
         text = f"La prière de {salat_list[number]} est dans {result}"
-        
     else:
         text = "Toutes les prières sont déjà passées"
     return play(text)
 
+def play_error(message):
+    """Function to play the error messages"""
+    path = "./error.mp3"
+    gTTS(message, lang="en").save(path)
+    playsound(path)
+    os.remove(path)
+
 def main():
+    """Main function"""
     try:
         os.remove(speak())
-    except:
-        path = "./error.mp3"
-        gTTS("An error occured, check for typos in the command line arguments or try again later", lang="en").save(path)
-        playsound(path)
-        os.remove(path)
+    except JSONDecodeError:
+        play_error("The city you have given is incorrect")
+    except ValueError:
+        play_error("The language you have given is incorrect")
 
 if __name__ == "__main__":
-   main()
+    main()
